4a8ffbd457ef6c2f296f028d189bda532c8b3145
lgw1226 <zczc156@gmail.com>
2024-01-08 14:52:45 +0900

    First working version of sac

--- main.py
+++ main.py
@@ -90,9 +90,6 @@ class Agent():
         self.device = kwargs.get('device')
         self.entropy_target = -act_dim
 
-        self.logstd_min = -5
-        self.logstd_max = 0
-
         self.p = MLP(obs_dim, act_dim * 2).to(device)
         self.p_optim = optim.Adam(self.p.parameters(), lr=lr)
 
@@ -111,12 +108,12 @@ class Agent():
 
         mean_logstd = self.p(o)
         mean = F.tanh(mean_logstd[:,:act_dim])
-        logstd = torch.clip(mean_logstd[:,act_dim:], self.logstd_min, self.logstd_max)
+        logstd = torch.clip(mean_logstd[:,act_dim:], -5, 0)
         dist = torch.distributions.Normal(mean, torch.exp(logstd))
 
         if not a:
             a = dist.rsample()
-        logp = dist.log_prob(a) - torch.log(1 + 1e-5 - torch.square(F.tanh(a))).sum(-1, keepdim=True)
+        logp = dist.log_prob(a) - torch.log(1 - F.tanh(a).square()).sum(-1, keepdim=True)
 
         return a, logp
     
@@ -130,15 +127,15 @@ class Agent():
 
         q1_sd = self.q1.state_dict()
         q1t_sd = self.q1t.state_dict()
-
         for key in q1_sd.keys():
             q1t_sd[key] = (1 - self.tau) * q1t_sd[key] + self.tau * q1_sd[key]
+        self.q1t.load_state_dict(q1t_sd)
 
         q2_sd = self.q2.state_dict()
         q2t_sd = self.q2t.state_dict()
-
         for key in q2_sd.keys():
             q2t_sd[key] = (1 - self.tau) * q2t_sd[key] + self.tau * q2_sd[key]
+        self.q2t.load_state_dict(q2t_sd)
     
     def update(self, o1, a, r, o2, d):
 
@@ -146,8 +143,8 @@ class Agent():
         a2, logp2 = self.get_action(o2)
         qt = r + self.gamma * (1 - d) * (torch.min(*self.get_value(o2, a2)) - alpha * logp2)
         q1, q2 = self.get_value(o1, a)
-        q1_loss = torch.square(q1 - qt.detach()).mean()
-        q2_loss = torch.square(q2 - qt.detach()).mean()
+        q1_loss = (q1 - qt.detach()).square().mean()
+        q2_loss = (q2 - qt.detach()).square().mean()
 
         self.q1_optim.zero_grad()
         q1_loss.backward()
@@ -163,7 +160,7 @@ class Agent():
             self.target_update_count = 0
 
         a1, logp1 = self.get_action(o1)
-        p_loss = -(torch.min(*self.get_value(o1, a1)) - alpha.detach() * logp1).mean()
+        p_loss = (alpha.detach() * logp1 - torch.min(*self.get_value(o1, a1))).mean()
 
         self.p_optim.zero_grad()
         p_loss.backward()
@@ -175,58 +172,74 @@ class Agent():
         alpha_loss.backward()
         self.alpha_optim.step()
 
-        return ((q1_loss + q2_loss) / 2).item(), p_loss.item(), alpha_loss.item(), alpha.mean().item()
+        return ((q1_loss + q2_loss) / 2).item(), p_loss.item(), alpha_loss.item(), alpha.item()
 
 
-wandb.init()
+def main(args):
 
-device = torch.device('cuda')
+    wandb.init()
 
-env_name = 'Pendulum-v1'
-env = gym.make(env_name)
+    device = torch.device('cuda')
 
-obs_dim = env.observation_space.shape[0]
-act_dim = env.action_space.shape[0]
-buffer = Buffer(obs_dim, act_dim, 1000000, device=device)
+    env_name = 'Pendulum-v1'
+    env = gym.make(env_name)
 
-agent = Agent(obs_dim, act_dim, lr=0.0003, device=device)
+    obs_dim = env.observation_space.shape[0]
+    act_dim = env.action_space.shape[0]
+    buffer = Buffer(obs_dim, act_dim, 1000000, device=device)
+    agent = Agent(obs_dim, act_dim, lr=0.0003, device=device)
 
-gradient_steps = 1
-total_gradient_steps = 0
-while total_gradient_steps <= 1000000:
+    gradient_steps = 1
+    total_gradient_steps = 0
+    while total_gradient_steps <= 1000000:
 
-    o, _ = env.reset()
-    episode_return = 0
-    while True:
+        o, _ = env.reset()
+        episode_return = 0
+        while True:
 
-        with torch.no_grad():
-            a, _ = agent.get_action(torch.as_tensor(o, device=device).unsqueeze(0))
-        o2, r, terminated, truncated, _ = env.step(a.ravel().cpu().numpy())
-        d = float(terminated or truncated)
+            with torch.no_grad():
+                a, _ = agent.get_action(torch.as_tensor(o, device=device).unsqueeze(0))
+            o2, r, terminated, truncated, _ = env.step(a.ravel().cpu().numpy())
+            d = float(terminated or truncated)
 
-        buffer.push(
-            torch.as_tensor(o, device=device),
-            torch.as_tensor(a, device=device),
-            torch.as_tensor(r, device=device),
-            torch.as_tensor(o2, device=device),
-            torch.as_tensor(d, device=device),
-        )
+            buffer.push(
+                torch.as_tensor(o, device=device),
+                torch.as_tensor(a, device=device),
+                torch.as_tensor(r, device=device),
+                torch.as_tensor(o2, device=device),
+                torch.as_tensor(d, device=device),
+            )
 
-        o = o2
-        episode_return += r
-        if d:
-            wandb.log({'EpisodeReturn': episode_return}, step=total_gradient_steps)
+            o = o2
+            episode_return += r
+            if d:
+                wandb.log({'EpisodeReturn': episode_return}, step=total_gradient_steps)
 
-            o, _ = env.reset()
-            episode_return = 0
-            break
+                o, _ = env.reset()
+                episode_return = 0
+                break
 
-    for _ in range(gradient_steps):
-        q_loss, p_loss, alpha_loss, alpha = agent.update(*buffer.sample(256))
-        wandb.log({
-            'Loss/Q': q_loss,
-            'Loss/P': p_loss,
-            'Loss/Alpha': alpha_loss,
-            'Alpha': alpha,
-        }, step=total_gradient_steps)
-        total_gradient_steps += 1
+        for _ in range(gradient_steps):
+            q_loss, p_loss, alpha_loss, alpha = agent.update(*buffer.sample(256))
+            wandb.log({
+                'Loss/Q': q_loss,
+                'Loss/P': p_loss,
+                'Loss/Alpha': alpha_loss,
+                'Alpha': alpha,
+            }, step=total_gradient_steps)
+            total_gradient_steps += 1
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('env-name', type=str)
+
+    args = parser.parse_args()
+
+    main(args)
+
+    pass
